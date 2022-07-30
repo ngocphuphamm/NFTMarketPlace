1a3280934adabac8f946790b38443464517462cd
ngocphuphamm <ngocphupham682001@gmail>
2022-07-30 12:42:21 +0700

    first commit

---
+++ contracts/NFT/Bin.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Bin is ERC721,Ownable{
+    using Counters for Counters.Counter;
+    
+    Counters.Counter private _tokenIdCount;
+    string private _baseTokenURI;
+    constructor() ERC721("Bin","PNP")
+    {
+        
+    }
+
+    function mint(address to) public onlyOwner returns(uint256)
+    {
+        _tokenIdCount.increment();
+        uint tokenId = _tokenIdCount.current();
+        _mint(to,tokenId);
+        return tokenId ; 
+    } 
+
+    function _baseURI() internal view virtual override returns(string memory )
+    {
+        return _baseTokenURI;
+    }
+
+    function _updateBaseURI(string memory baseUpdateURI) public  onlyOwner
+    {   
+        _baseTokenURI = baseUpdateURI;
+    }
+
+}
---
+++ test/Bin.test.js
@@ -0,0 +1,40 @@
+const {expect} = require("chai");
+const {ethers} = require("hardhat");
+
+describe("Bin NFT",()=>{
+    let [accountA,accountB,accountC] = [];
+    let contract ;
+    let address0 = "0x0000000000000000000000000000000000000000";
+    let uri = "sampleuri.com/";
+    beforeEach(async ()=>{
+        [accountA,accountB,accountC] = await ethers.getSigners();
+        const Contract = await ethers.getContractFactory("Bin");
+        contract = await Contract.deploy();
+        contract.deployed();
+    })
+    describe("mint",()=>{
+        it("should reveret if mint to zero address", async () =>{
+            await expect(contract.mint(address0)).to.be.revertedWith("ERC721: mint to the zero address");
+        })
+        it("should mint token correctly", async () =>{
+            let mintTx = await contract.mint(accountA.address);
+            await expect(mintTx).to.be.emit(contract,"Transfer").withArgs(address0,accountA.address,1);
+             expect(await contract.balanceOf(accountA.address)).to.be.equal(1);
+        
+        
+            let mintTx2 = await contract.mint(accountA.address);
+            await expect(mintTx2).to.be.emit(contract,"Transfer").withArgs(address0,accountA.address,2);
+             expect(await contract.balanceOf(accountA.address)).to.be.equal(2);
+        })
+    })  
+    describe("updateBaseTokenURI",()=>{
+        it("should update Base Token URI correctly", async () =>{
+            await contract.mint(accountA.address);
+            await contract._updateBaseURI(uri);
+            expect(await contract.tokenURI(1)).to.be.equal(uri + "1");
+        })
+    })
+
+
+
+})
